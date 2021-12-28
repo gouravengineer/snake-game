a7a7a186e9460a8c94d938fa1160d39b37ddac5e
gourav <gouravenginner@gmail.com>
2021-12-28 06:18:54 +0530

    snake can move up down left right

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from turtle import Turtle,Screen
+from snake import Snake
 import time
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 
 snake_game_on=False
+screen.tracer(0)        #It is used to turn off animation when arg*=0
+snake=Snake()
 
-snake=[]
-post=[0,-20,-40]
-for i in range(3):
-    tim=Turtle()
-    tim.color("white")
-    tim.shape("square")
-    tim.penup()
-    tim.setposition(post[i],0)
-    snake.append(tim)
+screen.listen()
+screen.onkey(fun=snake.up,key="Up")
+screen.onkey(fun=snake.down,key="Down")
+screen.onkey(fun=snake.right,key="Right")
+screen.onkey(fun=snake.left,key="Left")
+snake_game_on=True
+while snake_game_on:    #The while loop is used to move forward
+    time.sleep(0.1)
+    screen.update()
+    snake.move()
+    
 
+
+        
+    
        
         
 
