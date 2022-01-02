00a6960d91b5b7e3d6de82c95ee49fb01d5cc187
gourav <gouravenginner@gmail.com>
2022-01-02 12:59:19 +0530

    completed game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle,Screen
-from snake import Snake
+from snake import Snake,Food,Scoreboard
 import time
+
+score=Scoreboard()
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
@@ -8,7 +10,7 @@ screen.bgcolor("black")
 snake_game_on=False
 screen.tracer(0)        #It is used to turn off animation when arg*=0
 snake=Snake()
-
+food=Food()
 screen.listen()
 screen.onkey(fun=snake.up,key="Up")
 screen.onkey(fun=snake.down,key="Down")
@@ -16,13 +18,28 @@ screen.onkey(fun=snake.right,key="Right")
 screen.onkey(fun=snake.left,key="Left")
 snake_game_on=True
 while snake_game_on:    #The while loop is used to move forward
-    time.sleep(0.1)
+    time.sleep(0.055)
     screen.update()
     snake.move()
-    
 
+    #detect collision with food
+    if snake.head.distance(food)<14:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+    #detect collision with wall
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
+        snake_game_on=False
+        score.game_over()
 
-        
+    #detect collision with self
+    for segment in snake.snake_body[1:]:
+        if snake.head.distance(segment)<5:
+            snake_game_on=False
+            score.game_over()
+        #if head=snake_body[0] collides with any of the segment
+        #trigger game over
+
     
        
         
